7822e333e455a361318f872a05139934357a9714
Manuel Lindinger <manuel.lindinger@stud.edubs.ch>
2025-12-02 12:27:14 +0100

    java

--- Aufgaben/a1.java
+++ Aufgaben/a1.java
@@ -1,5 +1,26 @@
 package Aufgaben;
 
+import java.util.ArrayList;
+
 public class a1 {
-    
+
+    // Variable declarations
+    private String name;
+    private int age;
+    private double height;
+    private float weight;
+    private boolean isStudent;
+    private char grade;
+    private long idNumber;
+
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Hallo");
+        list.add("Welt");
+        list.add("!");
+
+        for (String s : list) {
+            System.out.print(s + " ");
+        }
+    }
 }

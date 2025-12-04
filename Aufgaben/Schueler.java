b873159b6299a0546206fb477985fc183014cd39
Manuel Lindinger <manuel.lindinger@stud.edubs.ch>
2025-12-04 09:38:57 +0100

    java

--- Aufgaben/Schueler.java
+++ Aufgaben/Schueler.java
@@ -3,7 +3,9 @@ package Aufgaben;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Schueler {
+
+
+public class Schueler{
 
     // Variable declarations
     private String name;
@@ -14,50 +16,77 @@ public class Schueler {
     private char grade;
     private long idNumber;
     static ArrayList<Integer> numList = new ArrayList<>();
+    public static String Debug = "[Debug]>>";
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
 
     public static void main(String[] args) {
-        /* ArrayList<String> list = new ArrayList<>();
-        
-        list.add("");
-        list.add("");
-        list.add("");
-
-        for (String s : list) {
-            System.out.print(s + " ");
-        } */
-        
+        /*
+         * ArrayList<String> list = new ArrayList<>();
+         * 
+         * list.add("");
+         * list.add("");
+         * list.add("");
+         * 
+         * for (String s : list) {
+         * System.out.print(s + " ");
+         * }
+         */
         Schueler s1 = new Schueler("Manuel", getRandomNumID(), true);
+        String m1 = s1.isStudent() ? (ANSI_GREEN + "Schueler wurde erstellt") : (ANSI_RED + "Lehrer wurde erstellt");
+        System.out.printf(Debug + m1 + ANSI_CYAN + "[%s, %d] %n" + ANSI_RESET, s1.getName(), s1.getNum());
         Schueler s2 = new Schueler("Finn", getRandomNumID(), true);
-
-        System.out.println("Schueler Nr." + s1.getNum() + ") " + s1.getName());
-        System.out.println("Schueler Nr." + s2.getNum() + ") " + s2.getName());
+        String m2 = s2.isStudent() ? (ANSI_GREEN + "Schueler wurde erstellt") : (ANSI_RED + "Lehrer wurde erstellt");
+        System.out.printf(Debug + m2 + ANSI_CYAN + "[%s, %d] %n" + ANSI_RESET, s2.getName(), s2.getNum());
+        Schueler s3 = new Schueler("Cleopatra", getRandomNumID(), false);
+        String m3 = s3.isStudent() ? (ANSI_GREEN + "Schueler wurde erstellt") : (ANSI_RED + "Lehrer wurde erstellt");
+        System.out.printf(Debug + m3 + ANSI_CYAN + "[%s, %d] %n" + ANSI_RESET, s2.getName(), s2.getNum());
     }
 
-        public static int getRandomNumID() {
+    public Schueler(String name) {
+        this.name = name;
+        this.idNumber = getRandomNumID();
+
+    }
+
+    public static int getRandomNumID() {
         Random rand = new Random();
         int num1 = rand.nextInt(1000);
         if (numList.contains(num1)) {
-            num1 = 0; 
+            num1 = 0;
         } else {
             numList.add(num1);
         }
         return num1;
     }
 
-    public long getNum(){
+    public long getNum() {
         return this.idNumber;
     }
-    
+
+    public boolean isStudent() {
+        return this.isStudent;
+    }
+
     public Schueler(String name, long idNumber, boolean isStudent) {
         this.name = name;
         this.idNumber = idNumber;
-        if(isStudent){
+        if (isStudent) {
             this.isStudent = true;
-        } else{
+        } else {
             this.isStudent = false;
         }
     }
-    public String getName(){
+
+    public String getName() {
         return this.name;
     }
 
@@ -65,27 +94,27 @@ public class Schueler {
         this.name = name;
     }
 
-    public void setage(int age){
+    public void setage(int age) {
         this.age = age;
     }
 
-    public void setheight(double height){
+    public void setheight(double height) {
         this.height = height;
     }
 
-    public void setweight(float weight){
+    public void setweight(float weight) {
         this.weight = weight;
     }
 
-    public void setisstudent(boolean isStudent){
+    public void setisstudent(boolean isStudent) {
         this.isStudent = isStudent;
     }
 
-    public void setGrade (char grade){
+    public void setGrade(char grade) {
         this.grade = grade;
     }
 
-    public void setidNumber(long idNumber){
+    public void setidNumber(long idNumber) {
         this.idNumber = idNumber;
     }
 }
